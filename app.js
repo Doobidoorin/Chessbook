f002314b1ed4fdbc47f972785f699a7ceb56d592
RIN <328751194+Doobidoorin@users.noreply.github.com>
2026-09-17 14:26:23 +0600

    Initialize Supabase client in app.js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
+const SUPABASE_URL = "https://zframxxhjajaweklenyz.supabase.co/rest/v1/";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_69jX0k_c1AkxonK_cjtrHQ_jZoRzuXf";
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
 
