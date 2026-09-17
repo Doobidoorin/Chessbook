2db63aaeaa21d68b01fe6bc1477392d1531bf4f8
RIN <328751194+Doobidoorin@users.noreply.github.com>
2026-09-17 15:16:12 +0600

    Rename supabase variable to supabaseClient

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const SUPABASE_URL = "https://zframxxhjajaweklenyz.supabase.co/rest/v1/";
 const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_69jX0k_c1AkxonK_cjtrHQ_jZoRzuXf";
 
-const supabase = window.supabase.createClient(
+const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_PUBLISHABLE_KEY
 );
